147b9c01cb62514879664636b90c6d98d3fe0f11
徐鵬鈞 <hsu.pengjun@outlook.com>
2019-12-06 05:39:42 +0800

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+    entry: ["./src/main.ts", "./styles/main.scss"],
+    plugins: [
+        new MiniCssExtractPlugin({ filename: 'styles/[name].[hash].css' }),
+        new HtmlWebpackPlugin({ template: './index.html' })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: [{ loader: MiniCssExtractPlugin.loader }, 'css-loader', 'sass-loader']
+            },
+            { test: /\.tsx?$/, loader: "ts-loader" },
+            {
+                test: /\.(png|jpe?g|gif)$/i,
+                loader: 'file-loader',
+                options: {
+                    name: '[contenthash].[ext]',
+                    outputPath: 'assets',
+                }
+            }
+        ]
+    },
+    output: {
+        publicPath: process.env.NODE_ENV === 'production' ? 'https://chen-wendy.github.io/resume/' : 'http://localhost:8000/',
+        path: path.resolve(__dirname, './dist'),
+        filename: 'scripts/[name].[hash].js'
+    }
+}
